404e8969fc74cec00014803ec4bbeccc01df8c90
Nazeh-Taha <nazeh3036@gmail.com>
2021-02-13 02:00:28 +0200

    Updates

---
+++ precache-manifest.de6183b9bbc8cbb11931dd935df9584d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd607024032342eef3d01bea8af7e0c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "05a1e9d1f08573be4009",
+    "url": "./static/css/main.8d31eeda.chunk.css"
+  },
+  {
+    "revision": "ac925daf4ec910b1946a",
+    "url": "./static/js/2.000168a6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.000168a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a1e9d1f08573be4009",
+    "url": "./static/js/main.d013ba4d.chunk.js"
+  },
+  {
+    "revision": "58d2f065a236588dfc0b",
+    "url": "./static/js/runtime-main.f7ecd8f0.js"
+  }
+]);
